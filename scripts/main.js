411c15560ef7fa32a2e118f718264f01d9871b9c
Matija Sirk <sirkmatija@gmail.com>
2017-01-08 13:11:53 +0100

    Added message change

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,18 @@
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello, World!';
+myHeading.textContent = 'Hello, World!'; // Change heading to "Hello, world!"
+
+/* Pop-up window with "Hey! :)" when user clicks page */
+document.querySelector('html').onclick = function() {
+    alert('Hey! :)');
+}
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() { // change image displayed when user clicks it
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/lambda.png') {
+	myImage.setAttribute('src', 'images/hawk.jpg');
+    } else {
+	myImage.setAttribute('src', 'images/lambda.png');
+    }
+}
